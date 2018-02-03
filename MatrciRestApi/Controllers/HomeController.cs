83894ed47d65f6e9c4674f5e6f380c48a10a63bb
dubetau <36101536+dubetau@users.noreply.github.com>
2018-02-03 13:09:08 +0200

    Add files via upload

---
+++ MatrciRestApi/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MatrciRestApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpPost]
+        public bool AddNewResult(string emis)//, [FromBody]string centreNo, [FromBody]string name, string wrote2014,
+            //[FromBody]string passed2014, [FromBody]string wrote2015, [FromBody]string passed2015, string wrote2016, string passed2016)
+        {
+            // write to the database
+            return true;
+        }
+
+        [HttpGet]
+        public string GetAllMatricResults()
+        {
+            // read all results from the database
+            return "tau";
+        }
+    }
+}
---
+++ MatrciRestApi/Models/ResultsCSVModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatrciRestApi.Models
+{
+    public class ResultsCSVModel
+    {
+        public string emis { get; set; }
+        public string centreNo { get; set; }
+        public string name { get; set; }
+        public string year { get; set; }
+        public string wrote2014 { get; set; }
+        public string passed2014 { get; set; }
+        public string wrote2015 { get; set; }
+        public string passed2015 { get; set; }
+        public string wrote2016 { get; set; }
+        public string passed2016 { get; set; }
+    }
+}
